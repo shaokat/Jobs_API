ab81e8868265a542380db2088084a16694e7b51c
shaokat <shaokatcse@gmail.com>
2021-11-30 22:31:20 +0100

    security packages added

--- app.js
+++ app.js
@@ -8,12 +8,31 @@ const jobsRouter = require('./routes/jobs')
 //connectDB
 const connectDB = require('./db/connect')
 
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 const authenticateUser = require('./middleware/authentication')
+
+
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
+
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(express.json());
 
 // routes
 app.use('/api/v1/auth', authRouter)
