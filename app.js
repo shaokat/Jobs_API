9b15faa83ac21eb3d11ad1f142de51afd13ed019
shaokat <shaokatcse@gmail.com>
2021-12-18 12:22:58 +0100

    swagger documentation added

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 require('dotenv').config();
 require('express-async-errors');
-const express = require('express');
-const app = express();
-const authRouter = require('./routes/auth')
-const jobsRouter = require('./routes/jobs')
-
-//connectDB
-const connectDB = require('./db/connect')
 
 // extra security packages
 const helmet = require('helmet');
@@ -14,11 +7,22 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+const express = require('express');
+const app = express();
+
+const connectDB = require('./db/connect');
+const authenticateUser = require('./middleware/authentication');
+// routers
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
-const authenticateUser = require('./middleware/authentication')
-
 
 app.set('trust proxy', 1);
 app.use(
@@ -27,25 +31,28 @@ app.use(
     max: 100, // limit each IP to 100 requests per windowMs
   })
 );
-
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
-app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
-app.use('/api/v1/auth', authRouter)
-app.use('/api/v1/jobs',authenticateUser, jobsRouter)
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI)
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
